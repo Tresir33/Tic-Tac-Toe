1c9f90be6e74fef173f6fefdad34bea9e2c9eec2
Tresir33 <tresilaho33@gmail.com>
2025-03-05 11:54:31 +0100

    Updated

---
+++ index.js
@@ -0,0 +1,19 @@
+// const gameBoard = document.querySelector("#gameboard")
+// const infoDisplay = document.querySelector("#info")
+// const startCells = [
+//     "", "", "",
+//     "", "", "",
+//     "", "", ""
+// ]
+
+// infoDisplay.textContent = "Circles goes first"
+
+
+// function createBoard() {
+//     startCells.forEach((cell, index) => {
+//             const cellElement = document.createElement("div")
+
+//             gameBoard.append(cellElement)
+
+//     })
+// }
